a22fcc68ec586a3334333be0f7254c46208651fb
yadukrishnan <yadhu4507@gmail.com>
2024-03-14 20:13:01 +0530

    users schema created

---
+++ models/usersSchema.js
@@ -0,0 +1,14 @@
+//users  schema 
+
+const mongoose =require('mongoose');
+const userSchema =new mongoose.Schema({
+userName:{type:String,require:true},
+firstName:{type:String,require:true},
+lastName:{type:String},
+DOB:{type:Date,require:true},
+email:{type:String,require:true},
+password:{type:String,require:true}
+},{timestamps:true});
+
+const usersModel = mongoose.model('users',userSchema);
+module.exports = usersModel;
